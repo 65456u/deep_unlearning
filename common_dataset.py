34b99d1637e66147b5da204bcc889bbddedbcd03
yhc <hongchen_yang@apple.com>
2025-01-24 18:52:46 +0800

    log

--- common_dataset.py
+++ common_dataset.py
@@ -15,7 +15,8 @@ def convert_raw_data_to_model_format(tokenizer, max_length, question, answer, mo
     num_question_tokens = len(tokenizer.tokenize(new_question, add_special_tokens=True))
     
     full_text = unlearning_target
-    print('FULL TEXT:',full_text)
+    # print('FULL TEXT:',full_text)
+    print(f"FULL TEXT: {full_text}")
     
     encoded = tokenizer(
         full_text, 
