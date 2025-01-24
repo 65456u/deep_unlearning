05950d2b2a3bfdd0136f2a2c47231519627784e4
yhc <hongchen_yang@apple.com>
2025-01-24 18:20:06 +0800

    set step

--- forget.py
+++ forget.py
@@ -82,14 +82,14 @@ def main(cfg):
     max_steps = int(num_epochs * len(torch_format_dataset)) // (batch_size * gradient_accumulation_steps * num_devices)
     print(f"max_steps: {max_steps}")
     print(f"steps_per_epoch: {steps_per_epoch}")
-    max_steps = 1000
+    max_steps = 1120
     print(f"max_steps: {max_steps}")
     training_args = transformers.TrainingArguments(
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         gradient_accumulation_steps=gradient_accumulation_steps,
         warmup_steps=max(1, steps_per_epoch),
-        # max_steps=max_steps,
+        max_steps=max_steps,
         num_train_epochs=num_epochs,
         learning_rate=lr,
         bf16=True,
