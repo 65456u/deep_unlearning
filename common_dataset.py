36f74686d047d55378f51aa03e790e5fa77f1a23
yhc <hongchen_yang@apple.com>
2025-01-24 16:45:08 +0800

    changed dataset

--- common_dataset.py
+++ common_dataset.py
@@ -14,6 +14,9 @@ def convert_raw_data_to_model_format(tokenizer, max_length, question, answer, mo
     full_text = new_question + new_answer
     num_question_tokens = len(tokenizer.tokenize(new_question, add_special_tokens=True))
     
+    full_text = answer
+    print(full_text)
+    
     encoded = tokenizer(
         full_text, 
         add_special_tokens=True, 
