bf747c98da685446ed3330e83e3642b7df15fece
yhc <hongchen_yang@apple.com>
2025-01-24 14:25:26 +0800

    update evaluation

--- vllm_eval.py
+++ vllm_eval.py
@@ -24,8 +24,13 @@ model_id = model_cfg["model_id"]
 #load vllm model
 model_eval = LLM(curr_save_dir, tokenizer=model_id, device="auto", dtype = "half",tensor_parallel_size=8)
 # eval_dataset = datasets.load_from_disk(curr_save_dir+"/eval.hf")
-eval_dataset_list = [Dataset.from_dict(torch.load("synthetic_data/family_relationships.pt")), Dataset.from_dict(torch.load("synthetic_data/family_biographies.pt"))]
-eval_dataset_name_list = ["relationships_", "biographies_"]
+
+# eval_dataset_list = [Dataset.from_dict(torch.load("synthetic_data/family_relationships.pt")), Dataset.from_dict(torch.load("synthetic_data/family_biographies.pt"))]
+# eval_dataset_name_list = ["relationships_", "biographies_"]
+
+full_dataset = Dataset.from_dict(torch.load("data/mhu_01_forget_dict.pt"))
+eval_dataset_list = [full_dataset]
+eval_dataset_name_list = ["forget_set"]
 
 #remove local model
 if args.clean_cache == "true":
@@ -36,7 +41,7 @@ Path(curr_save_dir).mkdir(parents=True, exist_ok=True)
 
 for eval_dataset, eval_dataset_name in zip(eval_dataset_list, eval_dataset_name_list):
     with torch.no_grad():
-        correct, responses = eval_qa_vllm(eval_dataset, model_eval, qk="question4", ak="answer4", question_start_tag=model_cfg["question_start_tag"], question_end_tag=model_cfg["question_end_tag"], answer_tag=model_cfg["answer_tag"])
+        correct, responses = eval_qa_vllm(eval_dataset, model_eval, qk='single_hop_question', ak='single_hop_answer', question_start_tag=model_cfg["question_start_tag"], question_end_tag=model_cfg["question_end_tag"], answer_tag=model_cfg["answer_tag"])
         torch.save(correct, f"{curr_save_dir}/{eval_dataset_name}correct.pt")
         torch.save(responses, f"{curr_save_dir}/{eval_dataset_name}responses.pt")
         acc = np.asarray(correct).astype(np.float32).mean()
